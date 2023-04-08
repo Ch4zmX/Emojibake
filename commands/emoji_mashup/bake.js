f8433777f52dfe9255960b1f36a749805a71c9eb
Sh4d0wb0Y <jaytripathi100@gmail.com>
2023-04-07 23:21:49 -0700

    add required command parameters

--- commands/emoji_mashup/bake.js
+++ commands/emoji_mashup/bake.js
@@ -3,7 +3,16 @@ const { SlashCommandBuilder } = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('bake')
-        .setDescription('Takes two emojis and mashes them together, returning an image of the output emoji'),
+        .setDescription('Takes two emojis and mashes them together, returning an image of the output emoji')
+        .addStringOption(option =>
+            option.setName('base_emoji')  // Add better name later
+            .setDescription('The base emoji to combine')
+            .setRequired(true))
+        .addStringOption(option =>
+            option.setName('secondary_emoji') // Add better name later
+            .setDescription('The emoji to combine the base with')
+            .setRequired(true)),
+            
     async execute(interaction) {
         await interaction.reply('hi');
         console.log(`User ${interaction.user.tag} used command ${interaction}`);
