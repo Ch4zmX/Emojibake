730c784a77c85ba1875cbd09ad0758ec677d5fdf
Sh4d0wb0Y <jaytripathi100@gmail.com>
2023-04-07 18:57:43 -0700

    add print statement showing who used what command

--- commands/emoji_mashup/bake.js
+++ commands/emoji_mashup/bake.js
@@ -6,6 +6,7 @@ module.exports = {
         .setDescription('Takes two emojis and mashes them together, returning an image of the output emoji'),
     async execute(interaction) {
         await interaction.reply('hi');
+        console.log(`User ${interaction.user.tag} used command ${interaction}`);
     },
 
 
